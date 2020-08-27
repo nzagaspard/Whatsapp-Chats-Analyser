a3c218e10e847af470122487b59b5b0fc7b18213
nzagaspard <65093165+nzagaspard@users.noreply.github.com>
2020-08-27 12:54:18 +0200

    change

--- setup.sh
+++ setup.sh
@@ -1,17 +1,13 @@
 mkdir -p ~/.streamlit/
 
-
+echo "\
+[general]\n\
+email = \"nzagaspard@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
 
 echo "\
-
 [server]\n\
-
 headless = true\n\
-
+enableCORS=false\n\
 port = $PORT\n\
-
-enableCORS = false\n\
-
-\n\
-
-" > ~/.streamlit/config.toml
+" > ~/.streamlit/config.toml 
